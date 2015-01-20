31d3886166625e55cef1385ffd505ad7ca8cbf12
cahavar <cahavar@55yali.com>
2015-01-20 18:13:20 +0800

    添加创建数据流程控制

--- test/api/create_test_data.js
+++ test/api/create_test_data.js
@@ -3,6 +3,8 @@
 var common = require('./common');
 var mongoose = common.mongoose;
 
+var async = require('async');
+
 /**
  * 生成公司数据
  * @param {Function} callback 形式为function(err, companies){}
@@ -82,6 +84,16 @@ var createCompanyProvokes = function (companyDataList, callback) {
   // todo
 };
 
+/**
+ * 让公司成员参加非跨公司挑战的活动。
+ * 不必所有成员都参加活动，不必所有活动都有成员参加。
+ * @param {Array} users 公司所有成员
+ * @param {Array} campaigns 所有活动
+ * @param {Function} callback 形式为function(err){}
+ */
+var addUsersToCampaigns = function (users, campaigns, callback) {
+  // todo
+};
 
 
 /**
@@ -89,6 +101,75 @@ var createCompanyProvokes = function (companyDataList, callback) {
  * @param {Function} callback 完成后的回调函数，形式为function(err){}
  */
 exports.createData = function (callback) {
-  // todo
-};
 
+  createCompanies(function (err, companies) {
+    // 为每个公司生成小队、用户、活动等数据
+    async.map(companies, function (company, mapCallback) {
+      async.waterfall([
+        function (waterfallCallback) {
+          // 生成小队、用户数据
+          async.parallel({
+            createTeams: function (parallelCallback) {
+              // 生成小队数据
+              createTeams(company, parallelCallback);
+            },
+            createUsers: function (parallelCallback) {
+              // 生成用户数据
+              createUsers(company, parallelCallback);
+            }
+          }, function (err, results) {
+            // 将小队和用户数据往下传递
+            waterfallCallback(err, {
+              teams: results.createTeams,
+              users: results.createUsers
+            });
+          });
+        },
+        function (data, waterfallCallback) {
+          // 用户加入小队
+          addUsersToTeams(data.users, data.teams, function (err) {
+            waterfallCallback(err, {
+              teams: data.teams,
+              users: data.users
+            });
+          });
+        },
+        function (data, waterfallCallback) {
+          // 生成除跨公司挑战外的活动
+          async.parallel({
+            createCompanyCampaigns: function (parallelCallback) {
+              createCompanyCampaigns(company, parallelCallback);
+            },
+            createTeamCampaigns: function (parallelCallback) {
+              createTeamCampaigns(company, data.teams, parallelCallback);
+            },
+            createInnerProvokes: function (parallelCallback) {
+              createInnerProvokes(company, data.teams, parallelCallback);
+            }
+          }, function (err, results) {
+            var campaigns = [];
+            campaigns.concat(results.createCompanyCampaigns, results.createTeamCampaigns, results.createInnerProvokes);
+            addUsersToCampaigns(data.users, campaigns, function (err) {
+              // 将小队数据传递给结果处理函数
+              waterfallCallback(err, data.teams);
+            });
+
+          });
+        }
+      ], function (err, result) {
+        // mapCallback 公司及小队数据，以便生成跨公司挑战
+        mapCallback(err, {
+          company: company,
+          teams: result
+        });
+      });
+    }, function (err, results) {
+      // 生成跨公司的挑战数据
+      createCompanyProvokes(results, function (err, campaigns) {
+        callback(err);
+      });
+    });
+
+  });
+
+};
