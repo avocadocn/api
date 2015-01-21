fa41075d3a97f5697873ef39970560232e11c73f
Maggie <maggie03230@gmail.com>
2015-01-21 15:48:07 +0800

    addUserToTeams修改

--- test/create_data/add_users_to_teams.js
+++ test/create_data/add_users_to_teams.js
@@ -30,17 +30,7 @@ var async = require('async');
  * @param {Object} companyData 公司数据
  * @param {Function} callback 执行完后的回调函数
  */
-//var addUsersToTeams = function (companyData, callback) {
-//
-//}
-
-/**
- * 让公司的部分员工加入几个小队
- * @param {Array} users 公司的所有成员
- * @param {Array} teams 公司的小队列表
- * @param {Function} callback 形式为function(err){}
- */
-var addUsersToTeams = function (users, teams, callback) {
+var addUsersToTeams = function (companyData, callback) {
   /**
    * 假设有四个人(传入多少人无所谓，只要>=4,剩下的都随机)，参加三个队(传入多少队无所谓)
    * A成为队1的队长，队2的队员；B成为队1的队员；C为队2的队长；D不参加任何队；其他人随机(都不是队长，为了不重复为队长)
@@ -51,9 +41,9 @@ var addUsersToTeams = function (users, teams, callback) {
   var random0or1 = function() {//随机出0或1
     return Math.floor(Math.random()*2);
   };
-  var userLength = users.length;
-  var teamLength = teams.length;
-  for(var i =users.length; i<userLength; i++) {//其他人数据初始化
+  var userLength = companyData.users.length;
+  var teamLength = companyData.teams.length;
+  for(var i =companyData.users.length; i<userLength; i++) {//其他人数据初始化
     relationship[i]=[];
     for(var j = 0; j<teamLength; j++){
       relationship[i].push(random0or1());
@@ -65,37 +55,37 @@ var addUsersToTeams = function (users, teams, callback) {
   for(var i=0; i<userLength; i++) {
     for(var j=0; j<teamLength; j++) {
       if(relationship[i][j]&&relationship[i][j]>0) {
-        users[i].team.push({
-          gid : teams[j].gid,
-          _id : teams[j]._id,
-          group_type : teams[j].group_type,
-          entity_type : teams[j].entity_type,
-          name : teams[j].name,
+        companyData.users[i].team.push({
+          gid : companyData.teams[j].gid,
+          _id : companyData.teams[j]._id,
+          group_type : companyData.teams[j].group_type,
+          entity_type : companyData.teams[j].entity_type,
+          name : companyData.teams[j].name,
           leader : relationship[i][j]===2,
-          logo : teams[j].logo
+          logo : companyData.teams[j].logo
         });
         if(relationship[i][j]===2) users[i].role = 'LEADER';
         var member =  {
-          _id : users[i]._id,
-          nickname: users[i].nickname,
-          photo: users[i].photo
+          _id : companyData.users[i]._id,
+          nickname: companyData.users[i].nickname,
+          photo: companyData.users[i].photo
         };
-        teams[j].member.push(member);
+        companyData.teams[j].member.push(member);
         if(relationship[i][j]===2) {
-          teams[j].leader.push(member);
+          companyData.teams[j].leader.push(member);
         }
       }
     }
-    users[i].save(function(err) {
+    companyData.users[i].save(function(err) {
       if(err) {
-        callback(err);
+        console.log(err);
       }
     });
   }
   for(var k =0; k<teamLength; k++) {
-    teams[k].save(function(err) {
+    companyData.teams[k].save(function(err) {
       if(err) {
-        callback(err);
+        console.log(err);
       }
     });
   }
