d61d57a11fee892b78bd62c5be84c6b756801e69
ytoon <yangtongbill@gmail.com>
2015-01-21 12:30:18 +0800

    更新公司、小队生成数据

--- test/create_data/create_companies.js
+++ test/create_data/create_companies.js
@@ -1,11 +1,56 @@
 'use strict';
 
+var chance = require('./chance.js');
 /**
  * 生成公司数据
  * @param {Function} callback 形式为function(err, companies){}
  */
-var createCompanies = function (callback) {
-  // todo
+var createCompanies = function(callback) {
+  var companies = [];
+  // The number of companies that you want to create
+  var num = 3;
+  for (var i = 0; i < num; i++) {
+    chance.generateCompanyData(function(err, result) {
+      var company = new Company({
+        username: data.username,
+        login_email: result.email,
+        email: {
+          domain: [result.email.split('@')[1]]
+        },
+        status: {
+          mail_active: true,
+          active: true
+        },
+        info: {
+          name: result.name,
+          city: {
+            province: result.province,
+            city: result.city,
+            district: result.district
+          },
+          address: result.address,
+          lindline: {
+            areacode: result.areacode,
+            number: result.tel,
+            extension: result.extension
+          },
+          linkman: result.contacts,
+          phone: result.phone,
+          email: result.email
+        }
+      });
+
+      // Insert the company data to MongoDB 
+      company.save(function(err) {
+        if (err) console.log(err);
+        process.exit(0);
+      });
+
+      companies.push(company);
+    });
+  }
+  callback(null, companies);
+
 };
 
 module.exports = createCompanies;
--- test/create_data/create_teams.js
+++ test/create_data/create_teams.js
@@ -1,12 +1,50 @@
 'use strict';
 
+var chance = require('./chance.js');
+
 /**
  * 为公司生成小队
  * @param {Object} company
  * @param {Function} callback 形式为function(err, teams){}
  */
-var createTeams = function (company, callback) {
-  // todo
+var createTeams = function(company, callback) {
+  var teams = [];
+  // The number of teams that you want to create
+  var num = 10;
+  for (var i = 0; i < company.length; i++) {
+    for (var j = 0; j < num; j++) {
+      chance.generateTeamData(function(err, result) {
+        var team = new CompanyGroup({
+          cid: company[i]._id,
+          gid: result.gid,
+          poster: {
+            role: 'HR'
+          },
+          group_type: result.group_type,
+          cname: company[i].name,
+          name: result.name,
+          entity_type: result.entity_type,
+          brief: result.brief,
+          city: {
+            province: company[i].info.city.province,
+            city: company[i].info.city.city,
+            district: company[i].info.city.district
+          }
+        });
+
+        // Insert the company data to MongoDB 
+        team.save(function(err) {
+          if (err) console.log(err);
+          process.exit(0);
+        });
+
+        teams.push(team);
+      });
+    }
+  }
+
+  callback(null, teams);
+
 };
 
 module.exports = createTeams;
