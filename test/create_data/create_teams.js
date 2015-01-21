d39eb2577f5fa36058fc5575605e026b6e566bb5
ytoon <yangtongbill@gmail.com>
2015-01-21 15:12:16 +0800

    更新小队生成文件

--- test/create_data/create_teams.js
+++ test/create_data/create_teams.js
@@ -11,10 +11,20 @@ var chance = require('./chance.js');
 var createTeams = function(company, callback) {
   var teams = [];
   // The number of teams that you want to create
-  var num = 10;
+  var num = 6;
   for (var i = 0; i < company.length; i++) {
     for (var j = 0; j < num; j++) {
       chance.generateTeamData(function(err, result) {
+        if (j == 0) {
+          result.gid = 3;
+          result.group_type = '阅读';
+          result.entity_type = 'Reading';
+        }
+        if (j >= 1 && j <= 3) {
+          result.gid = 7;
+          result.group_type = '足球';
+          result.entity_type = 'FootBall';
+        }
         var team = new CompanyGroup({
           cid: company[i]._id,
           gid: result.gid,
