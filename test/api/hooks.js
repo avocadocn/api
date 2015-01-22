2307eaccdf19e52e357a2d68134f686d47215bec
cahavar <cahavar@55yali.com>
2015-01-22 13:16:19 +0800

    移除不正确的删除数据库代码

--- test/api/hooks.js
+++ test/api/hooks.js
@@ -1,22 +1,13 @@
 var createDataModule = require('../create_data');
-var common = require('../support/common.js');
-var mongoose = require('mongoose');
 
 before(function (done) {
   this.timeout(10 * 1000);
-  mongoose.connection.db.dropDatabase(function (err, result) {
+
+  createDataModule.createData(function (err) {
     if (err) {
       console.log(err.stack);
     }
-    console.log(result);
-
-    createDataModule.createData(function (err) {
-      if (err) {
-        console.log(err.stack);
-      }
-      done();
-    });
-
+    done();
   });
 
 });
