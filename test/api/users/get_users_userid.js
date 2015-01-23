d7b93c8f9c4a3a0d9d2fca6ee61cf3a96f641cda
cahavar <cahavar@55yali.com>
2015-01-23 11:15:52 +0800

    生成被关闭、未激活的用户；完善用户登录测试用例

---
+++ test/api/users/get_users_userid.js
@@ -0,0 +1,14 @@
+var app = require('../../../config/express.js'),
+  request = require('supertest')(app);
+
+var dataService = require('../../create_data');
+var common = require('../../support/common');
+
+module.exports = function () {
+
+  describe('get /users/:userId', function () {
+
+
+  });
+
+};
