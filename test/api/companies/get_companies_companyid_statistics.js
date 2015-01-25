846b2a6fd989c2674e218dc6394466ed68d73359
Maggie <maggie03230@gmail.com>
2015-01-25 12:59:48 +0800

    继续增加companies部分的测试

--- test/api/companies/get_companies_companyid_statistics.js
+++ test/api/companies/get_companies_companyid_statistics.js
@@ -1,10 +1,7 @@
 var app = require('../../../config/express.js'),
   request = require('supertest')(app);
 
-var util = require('util');
-var common = require('../../support/common');
 var dataService = require('../../create_data');
-var chance = require('chance').Chance();
 var tools = require('../../../tools/tools.js');
 
 module.exports = function () {
--- test/api/companies/post_companies_login.js
+++ test/api/companies/post_companies_login.js
@@ -42,6 +42,39 @@ module.exports = function () {
       });
     });
 
+    it('公司用户名和密码正确加device信息应该登录成功', function (done) {
+      var company = data[0].model;
+
+      request.post('/companies/login')
+        .set('x-app-id', "id1a2b3c4d5e6f")
+        .set('x-api-key', "key1a2b3c4d5e6f")
+        .set('x-device-id', "429F38FB-35FD-49ED-8E90-B5ACA9FC7468")
+        .set('x-device-type', "iPhone5,2")
+        .set('x-platform', "iOS")
+        .set('x-version', "8.1.2")
+        .send({
+          username: company.username,
+          password: '55yali'
+        })
+        .expect(200)
+        .end(function (err, res) {
+          if (err) return done(err);
+          res.body.id.should.equal(company._id.toString());
+
+          jwt.verify(res.body.token, common.config.token.secret, function (err, decoded) {
+            if (err) {
+              console.log(err);
+              err.should.not.be.ok;
+            } else {
+              decoded.id.should.equal(company._id.toString());
+              decoded.type.should.equal('company');
+            }
+            done();
+          });
+
+        });
+    });
+
     var loginErrorTest = function(theme, testData, msg) {
       var title = util.format('%s应该登录失败', theme)
       it(title, function (done) {
