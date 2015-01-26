37a2b3bcc47901013ab190a365f174ea0f4057ac
ytoon <yangtongbill@gmail.com>
2015-01-26 17:50:14 +0800

    modify get_groups.js and add get_teams_teamId_tags.js, get_teams_lead_list.js, get_teams_teamId_members.js

---
+++ test/api/teams/ get_teams_teamId_tags.js
@@ -0,0 +1,143 @@
+// Copyright by ytoon, 2015/01/26
+// Test the get teams/:teamId/tags routes.
+// 
+'use strict';
+
+var app = require('../../../config/express.js'),
+  request = require('supertest')(app);
+
+var dataService = require('../../create_data');
+
+
+module.exports = function() {
+
+  describe('get /teams/:teamId/tags', function() {
+    var userAccessToken = [,];
+    var companyAccessToken;
+
+    before(function(done) {
+      var data = dataService.getData();
+      var company = data[0].model;
+      var user = data[0].users[0];
+
+      request.post('/users/login')
+        .send({
+          email: user.email,
+          password: '55yali'
+        })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            console.log(res.body);
+            return done(err);
+          }
+          userAccessToken[0] = res.body.token;
+          //done();
+        });
+
+        var user = data[0].users[1];
+        request.post('/users/login')
+        .send({
+          email: user.email,
+          password: '55yali'
+        })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            console.log(res.body);
+            return done(err);
+          }
+          userAccessToken[1] = res.body.token;
+          //done();
+        });
+
+        request.post('/companies/login')
+        .send({
+          username: company.username,
+          password: '55yali'
+        })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            console.log(res.body);
+            return done(err);
+          }
+          companyAccessToken = res.body.token;
+          done();
+        });
+
+
+    });
+
+    it('有效用户获取所在小队标签信息', function(done) {
+      var data = dataService.getData();
+      var user = data[0].users[0];
+
+      request.get('/teams/'+ user.team[0]._id + '/tags')
+        .set('x-access-token', userAccessToken[0])
+        .expect(200)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          res.body.should.be.type('object');
+          done();
+        });
+    });
+
+    it('有效用户获取所在公司其他小队标签信息', function(done) {
+      var data = dataService.getData();
+      var user = data[0].users[2];
+      request.get('/teams/'+ user.team[0]._id + '/tags')
+        .set('x-access-token', userAccessToken[1])
+        .expect(200)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          
+          done();
+        });
+    });
+
+    // Produre Error: a user can't get tags of teams of other company.
+    it('有效用户获取其他公司小队标签信息，权限不够', function(done) {
+      var data = dataService.getData();
+      var user = data[0].users[0];
+      request.get('/teams/'+ data[1].users[0].team[0]._id + '/tags')
+        .set('x-access-token', userAccessToken[0])
+        .expect(403)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          
+          done();
+        });
+    });
+
+    it('有效公司获取其公司小队标签信息', function(done) {
+      var data = dataService.getData();
+      var user = data[0].users[0];
+
+      request.get('/teams/'+ user.team[0]._id + '/tags')
+        .set('x-access-token', companyAccessToken)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          
+          res.body.should.be.type('object');
+          done();
+        });
+    });
+    // Produre Error: a company can't get tags of teams of other company.
+    it('有效公司获取其他公司小队标签信息，权限不够', function(done) {
+      var data = dataService.getData();
+      var user = data[1].users[0];
+
+      request.get('/teams/'+ user.team[0]._id + '/tags')
+        .set('x-access-token', companyAccessToken)
+        .expect(403)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          
+          done();
+        });
+    });
+
+  });
+};
---
+++ test/api/teams/get_teams_lead_list.js
@@ -0,0 +1,75 @@
+// Copyright by ytoon, 2015/01/26
+// Test the get goups routes.
+// 
+'use strict';
+
+var app = require('../../../config/express.js'),
+  request = require('supertest')(app);
+
+var dataService = require('../../create_data');
+
+
+module.exports = function() {
+
+  describe('get /teams/lead/list', function() {
+    var userAccessToken;
+
+    before(function(done) {
+      var data = dataService.getData();
+      var company = data[0].model;
+      var user = data[0].users[0];
+
+      request.post('/users/login')
+        .send({
+          email: user.email,
+          password: '55yali'
+        })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            console.log(res.body);
+            return done(err);
+          }
+          userAccessToken = res.body.token;
+          done();
+        });
+    });
+
+    it('有效用户获取作为队长身份的小队', function(done) {
+      request.get('/teams/lead/list')
+        .set('x-access-token', userAccessToken)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          res.body[0]._id.should.be.type('string');
+          done();
+        });
+    });
+
+    it('有效用户获取指定类型的小队', function(done) {
+      var data = dataService.getData();
+      var user = data[0].users[0];
+      // 此类型：用户为队长
+      request.get('/teams/lead/list' + '?gid=' + user.team[0].gid)
+        .set('x-access-token', userAccessToken)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          res.body[0]._id.should.be.type('string');
+          //done();
+        });
+        // 此类型：用户为非队长
+        request.get('/teams/lead/list' + '?gid=' + user.team[1].gid)
+        .set('x-access-token', userAccessToken)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) console.log(err);
+          res.body.should.be.empty;
+          done();
+        });
+
+    });
+
+
+  });
+};
