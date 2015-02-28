94ea65c0b7a17430cefdee778033a9c12f06dce2
cahavar <cahavar@55yali.com>
2015-02-28 13:28:15 +0800

    添加部门api

--- controllers/departments.js
+++ controllers/departments.js
@@ -2,10 +2,12 @@
 
 var mongoose = require('mongoose');
 var Department = mongoose.model('Department'),
-    Company = mongoose.model('Company'),
-    donlerValidator = require('../services/donler_validator.js'),
-    log = require('../services/error_log.js'),
-    auth = require('../services/auth.js');
+  Company = mongoose.model('Company'),
+  CompanyGroup = mongoose.model('CompanyGroup'),
+  donlerValidator = require('../services/donler_validator.js'),
+  log = require('../services/error_log.js'),
+  auth = require('../services/auth.js'),
+  StackAndQueue = require('../tools/stack.js');
 /**
  * 获取部门树的所有部门的_id，并push进数组中，返回该数组
  *
@@ -106,7 +108,7 @@ var sendDepartments = function(company, department_ids, res) {
 module.exports = function (app) {
 
   return {
-    getDepartment: function(req, res) {
+    getDepartmentTree: function(req, res) {
       Company.findOne({
         _id: req.params.companyId,
         'status.active': true
@@ -147,6 +149,359 @@ module.exports = function (app) {
           res.sendStatus(500);
         });
       }
+    },
+
+    createDepartment: function (req, res, next) {
+      // todo 从yali原封不动移到这里，现在可用，但应该改进这部分代码
+      if (req.user.provider === 'company') {
+        var did = req.body.did;
+        var name = req.body.name;
+        var cid = req.body.cid;
+
+        if (req.user._id.toString() !== cid) {
+          res.status(403);
+          next('forbidden');
+          return;
+        }
+
+        var team_create = {
+          'cid': req.user._id,
+          'gid': '0',
+          'group_type': 'virtual',
+          'name':req.body.name,
+          'cname': req.user.info.name,
+          'entity_type': 'virtual'
+        }
+        CompanyGroup.create(team_create, function(err, company_group) {
+          if (err || !company_group) {
+            res.send({
+              'msg': 'TEAM_CREATE_FAILURE'
+            });
+          } else {
+            var department_create = {
+              'parent_department': did,
+              'name': name,
+              'company': {
+                '_id': req.user._id,
+                'name': req.user.info.name,
+                'logo': req.user.info.logo
+              },
+              'team': company_group._id
+            };
+            Department.create(department_create, function(err, department) {
+              if (err || !department) {
+                res.send({
+                  'msg': 'DEPARTMENT_CREATE_FAILURE'
+                });
+              } else {
+                Company.findOne({
+                  '_id': req.user._id
+                }, function(err, company) {
+                  if (err || !company) {
+                    res.send({
+                      'msg': 'DEPARTMENT_UPDATE_FAILURE'
+                    });
+                  } else {
+                    var child = {
+                      '_id': department._id,
+                      'level':0,
+                      'name': name,
+                      'department': []
+                    };
+                    var param = {
+                      'type': 0,
+                      'child': child
+                    };
+                    company.department = departmentFindAndUpdate(req.user, did, param).department;
+
+                    company.save(function(err) {
+                      if (err) {
+                        res.send({
+                          'msg': 'DEPARTMENT_UPDATE_FAILURE'
+                        });
+                      } else {
+                        res.send({
+                          '_id': req.user._id,
+                          'name': req.user.info.name,
+                          'department': company.department
+                        });
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+      } else {
+        res.status(403);
+        next('forbidden');
+        return;
+      }
+    },
+
+    getDepartment: function (req, res, next) {
+      Department.findOne({
+        '_id': req.params.departmentId,
+        'status':{'$ne':'delete'}
+      }).populate('team').exec(function(err, department) {
+        if (err || !department) {
+          res.send(500, {
+            'department': null
+          });
+        } else {
+          res.send(200, {
+            'department': department
+          })
+        }
+      });
+    },
+
+    updateDepartment: function (req, res, next) {
+      // todo 可用，但需要改进
+      if (req.role !== 'HR') {
+        res.status(403);
+        next('forbidden');
+        return;
+      }
+
+      var did = req.body.did;
+      var name = req.body.name;
+      Company.findOne({
+        '_id': req.user._id
+      }, function(err, company) {
+        if (err || !company) {
+          res.send({
+            'msg': 'DEPARTMENT_UPDATE_FAILURE'
+          });
+        } else {
+          var param = {
+            'type': 1,
+            'name': name
+          };
+          company.department = departmentFindAndUpdate(req.user, did, param).department;
+          company.save(function(err) {
+            if (err) {
+              res.send({
+                'msg': 'DEPARTMENT_UPDATE_FAILURE'
+              });
+            } else {
+
+              Department
+                .findById(did)
+                .populate('team')
+                .exec()
+                .then(function(department) {
+                  department.name = name;
+                  department.save(function(err) {
+                    if (err) {
+                      console.log(err);
+                      res.send(500);
+                    } else {
+                      department.team.name = name;
+                      department.team.save(function(err) {
+                        if (err) {
+                          console.log(err);
+                          res.send(500);
+                        } else {
+                          User.update(
+                            {'department._id': department._id},
+                            {'department.name': name},
+                            {'safe': false, 'multi': true},
+                            function(err, users) {
+                              if (err) {
+                                console.log(err);
+                                res.send(500);
+                              } else {
+                                res.send({
+                                  'msg': 'DEPARTMENT_UPDATE_SUCCESS',
+                                  '_id': company._id,
+                                  'name': company.info.name,
+                                  'department': company.department
+                                });
+                              }
+                            });
+                        }
+                      });
+                    }
+                  });
+                })
+                .then(null, function(err) {
+                  console.log(err);
+                  res.send(500);
+                });
+            }
+          });
+        }
+      });
+    },
+
+    deleteDepartment: function (req, res, next) {
+      // todo 可用，但需要改进
+      if (req.role !== 'HR') {
+        res.status(403);
+        next('forbidden');
+        return;
+      }
+
+      var did = req.params.departmentId;
+      if (did.toString() === req.user._id.toString()) {
+        //删除该公司下的所有部门
+        deleteFromRoot(req.user, -1, req, res);
+      } else {
+        //删除某个部门以及其下所有子部门
+        operateFromRootAndDeleteOne(did, req, res);
+      }
     }
+
   }
 };
+
+function deleteFromRoot(department, seq, req, res) {
+  var stack = new StackAndQueue.stack();
+  var delete_ids = [];
+
+  //删除某个部门以及其下的所有部门
+  if (seq != -1) {
+    delete_ids.push(department[seq]._id);
+    stack.push({
+      '_id': department[seq]._id,
+      'department': department[seq].department
+    });
+    //删除公司下的所有部门
+  } else {
+    stack.push({
+      '_id': department._id,
+      'department': department.department
+    });
+  }
+  while (!stack.isEmpty()) {
+    var pop = stack.pop();
+    if (pop.department.length > 0) {
+      for (var i = 0; i < pop.department.length; i++) {
+        //待删除的部门id
+        delete_ids.push(pop.department[i]._id);
+        stack.push(pop.department[i]);
+      }
+    }
+  }
+
+  //员工的部门、小队也要删掉
+  Department.find({
+    '_id': {
+      '$in': delete_ids
+    }
+  }, function(err, departments) {
+    var user_ids = [];
+    var team_ids = [];
+    if (departments) {
+      for (var i = 0; i < departments.length; i++) {
+        for (var j = 0; j < departments[i].member.length; j++) {
+          user_ids.push(departments[i].member[j]._id);
+        }
+        team_ids.push(departments[i].team);
+      }
+
+      User.update({
+        '_id': {
+          '$in': user_ids
+        }
+      }, {
+        '$set': {
+          'department': undefined
+        },
+        '$pull':{
+          'team':{
+            'gid':'0'
+          }
+        }
+      }, {
+        'multi': true
+      }, function(err, users) {
+        Department.update({
+          '_id': {
+            '$in': delete_ids
+          }
+        },{'$set':{'status':'delete'}},{'multi':true}, function(err, _department) {
+          if (err || !_department) {
+            return res.send({
+              'msg': 'DEPARTMENT_DELETE_FAILURE',
+              'department': []
+            });
+          } else {
+
+            if (seq != -1) {
+              department.splice(seq, 1);
+            } else {
+              req.user.department = [];
+            }
+
+            Company.findOne({
+              '_id': req.user._id
+            }, function(err, company) {
+              if (err || !company) {
+                res.send({
+                  'msg': 'DEPARTMENT_DELETE_FAILURE',
+                  'department': []
+                });
+              } else {
+                company.department = req.user.department;
+                company.save(function(err) {
+                  if (err) {
+                    res.send({
+                      'msg': 'DEPARTMENT_DELETE_FAILURE',
+                      'department': []
+                    });
+                  } else {
+                    res.send({
+                      'msg': 'DEPARTMENT_DELETE_SUCCESS',
+                      '_id': req.user._id,
+                      'name': req.user.info.name,
+                      'department': company.department
+                    });
+                  }
+                })
+              }
+            })
+          }
+        });
+      });
+    }
+  });
+}
+
+function operateFromRootAndDeleteOne(did, req, res) {
+  var stack = new StackAndQueue.stack();
+  var find = false;
+  //从根部开始找
+  stack.push({
+    '_id': req.user._id,
+    'department': req.user.department
+  });
+  while (!stack.isEmpty() && !find) {
+    var pop = stack.pop();
+    if (pop.department.length > 0) {
+      for (var i = 0; i < pop.department.length && !find; i++) {
+
+        if (pop.department[i]._id.toString() === did.toString()) {
+          find = true;
+
+          //pop.department.splice(i,1);
+
+          deleteFromRoot(pop.department, i, req, res);
+          return;
+
+        } else {
+          stack.push(pop.department[i]);
+        }
+
+      }
+    }
+  }
+  if (!find) {
+    return res.send({
+      'msg': 'DEPARTMENT_DELETE_SUCCESS',
+      'department': req.user.department
+    });
+  }
+}
--- routes/departments.js
+++ routes/departments.js
@@ -4,6 +4,12 @@ var token = require('../services/token.js');
 var getById = require('../middlewares/getById.js');
 
 module.exports = function (app, ctrl) {
-  app.get('/departmentTree/:companyId',  token.needToken, ctrl.getDepartment);
-  app.get('/departmentTree/:companyId/detail',  token.needToken, ctrl.getDepartmentTreeDetail);
+  app.get('/departmentTree/:companyId', token.needToken, ctrl.getDepartmentTree);
+  app.get('/departmentTree/:companyId/detail', token.needToken, ctrl.getDepartmentTreeDetail);
+
+  app.post('/department', token.needToken, ctrl.createDepartment);
+  app.get('/department/:departmentId', token.needToken, ctrl.getDepartment);
+  app.put('/department/:departmentId', token.needToken, ctrl.updateDepartment);
+  app.delete('/department/:departmentId', token.needToken, ctrl.deleteDepartment);
+
 };
