0a203229aaba2b3efc056a13c4c997200d181119
cahavar <cahavar@55yali.com>
2015-03-02 10:35:54 +0800

    添加搜索api

--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var token = require('../services/token');
+
 module.exports = function (app, ctrl) {
   app.post('/search/companies', ctrl.searchCompanies);
+  app.post('/search/users', token.needToken, ctrl.searchUsers);
 };
