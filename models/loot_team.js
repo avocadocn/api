06b48c6f0cdd5af41aa9603356769d63ca4b46ef
cahavar <cahavar@55yali.com>
2015-03-06 14:25:48 +0800

    添加LootTeam模型

---
+++ models/loot_team.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var LootTeam = new Schema({
+  loot_id: Schema.Types.ObjectId, // 某个具体场地争夺机会的_id，对应于模型Loot
+  team: {
+    type: Schema.Types.ObjectId,
+    ref: 'CompanyGroup'
+  },
+
+  // 参与争夺场地的成员
+  loot_members: [{
+    _id: Schema.Types.ObjectId,
+    is_success: Boolean // 是否争取成功
+  }]
+
+});
+
+mongoose.model('LootTeam', LootTeam);
