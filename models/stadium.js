9f373d237257b390b8545cf93304f5e8c6f62a29
cahavar <cahavar@55yali.com>
2015-03-06 14:05:01 +0800

    添加场馆的模型

---
+++ models/stadium.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var Stadium = new Schema({
+  // 场馆名称
+  name: {
+    type: String,
+    required: true
+  },
+
+  // 精确地址
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [],
+    name: String // 详细地址
+  },
+
+  // 同Group的group_type属性
+  group_type: String,
+
+  // 简介
+  introduce: String
+});
+
+mongoose.model('Stadium', Stadium);
