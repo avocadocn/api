5f00c0d3fd2ddbaf66633bd59f9805b7cd53b822
cahavar <cahavar@55yali.com>
2015-03-17 17:04:06 +0800

    实现上传文件控制器中的若干函数

--- controllers/file.js
+++ controllers/file.js
@@ -12,7 +12,8 @@ var multiparty = require('multiparty'),
   gm = require('gm'),
   mime = require('mime'),
   mkdirp = require('mkdirp'),
-  async = require('async');
+  async = require('async'),
+  moment = require('moment');
 
 // mongoose and models
 var mongoose = require('mongoose'),
@@ -48,11 +49,16 @@ module.exports = function (app) {
       form.parse(req, parseForm);
 
       function parseForm(err, fields, files) {
+        if (err) {
+          next(err);
+          return;
+        }
 
         var data = {};
 
-        async.waterfall([
+        async.series([
           function (callback) {
+            // 权限验证
             if (!fields['owner'][0]) {
               res.status(400).send({msg: '上传文件须指定所属对象'});
               callback('break');
@@ -74,18 +80,18 @@ module.exports = function (app) {
             });
           },
           function (callback) {
-            // TODO: 将文件从临时目录移到存档目录(并设为只读，不可写和执行)
+            // 将文件从临时目录移到存档目录(并设为只读，不可写和执行)
             if (!files['files']) {
               res.status(400).send({msg: '没有上传文件'});
               callback('break');
               return;
             }
-            moveFilesFromTempDirToBackupDir(files['files'], function (err, resFiles) {
+            moveFilesFromTempDirToBackupDir(files['files'], function (err, fileDatas) {
               if (err) {
                 callback(err);
               }
               else {
-                data.resFiles = resFiles;
+                data.fileDatas = fileDatas;
                 callback();
               }
             });
@@ -95,7 +101,7 @@ module.exports = function (app) {
             callback(); // TODO
           },
           function (callback) {
-            // TODO: 新建并保存数据模型
+            // 新建并保存数据模型
             var uploader = {
               _id: req.user._id,
               kind: req.user.provider,
@@ -105,11 +111,11 @@ module.exports = function (app) {
               kind: fields['owner'][0].kind,
               _id: fields['owner'][0]._id
             };
-            data.resFiles.forEach(function (fileData) {
+            data.fileDatas.forEach(function (fileData) {
               fileData.uploader = uploader;
               fileData.owner = owner;
             });
-            createAndSaveFileModel(data.resFiles, callback);
+            createAndSaveFileModel(data.fileDatas, callback);
           }
         ], function (err, results) {
           if (err) {
@@ -142,27 +148,108 @@ module.exports = function (app) {
  */
 function authUpload(owner, user, callback) {
   // TODO
+  callback(null, true);
 }
 
 /**
- * 将上传的文件从临时目录移动到备份目录
+ * 将多个上传的文件从临时目录移动到备份目录
  *
  * example:
- *  moveFilesFromTempDirToBackupDir(files, function (err, files) {
- *
+ *  moveFilesFromTempDirToBackupDir(files, function (err, fileDatas) {
+ *    // do something
  *  });
  *
- *  files是一个数组: [{
+ *  fileDatas是一个数组: [{
  *    originName: String, // 上传的原文件名
  *    name: String, // 重命名后的文件名
  *    path: String // 保存后的路径，相对于yali网站目录的路径，如'/backup/2015/03/cid/test.png'
  *  }]
  *
  * @param {Array} files 文件数组
- * @param {Function} callback 形式为function (err, files) {}
+ * @param {Function} callback 形式为function (err, fileDatas) {}
  */
 function moveFilesFromTempDirToBackupDir(files, callback) {
-  // TODO
+  async.map(files, function (file, mapCallback) {
+    moveFileFromTempDirToBackupDir(file, mapCallback);
+  }, callback);
+}
+
+/**
+ * 将一个上传的文件从临时目录移动到备份目录
+ *
+ * example:
+ *  moveFileFromTempDirToBackupDir(files, function (err, resFileData) {
+ *    // do something
+ *  });
+ *
+ *  resFileData: {
+ *    originName: String, // 上传的原文件名
+ *    name: String, // 重命名后的文件名
+ *    path: String // 保存后的路径，相对于yali网站目录的路径，如'/backup/2015/03/cid/test.png'
+ *  }
+ *
+ * @param {Object} file 文件数据
+ * @param {ObjectId|String} cid 上传者所属的公司id
+ * @param {Function} callback 形式为function (err, resFileData) {}
+ */
+function moveFileFromTempDirToBackupDir(file, cid, callback) {
+  var resFileData;
+  var backupDir = path.join(yaliDir, 'backup', createDateCidDir(cid));
+  var newDateName = createNewDateName(file);
+  async.series([
+    function (seriesCallback) {
+      // create backup dir if not exists
+      fs.exists(backupDir, function (err, isExists) {
+        if (err) {
+          seriesCallback(err);
+        }
+        else {
+          if (isExists) {
+            seriesCallback();
+          }
+          else {
+            mkdirp(backupDir, seriesCallback);
+          }
+        }
+      });
+
+    },
+    function (seriesCallback) {
+      fs.rename(file.path, path.join(backupDir, newDateName), seriesCallback);
+    }
+  ], function (err, results) {
+    if (err) {
+      callback(err);
+    }
+    else {
+      resFileData = {
+        originName: file.originalFilename,
+        name: newDateName,
+        path: backupDir
+      };
+      callback(null, resFileData);
+    }
+  });
+}
+
+/**
+ * 创建带日期和公司id的路径
+ * @param {Object|String} cid
+ * @returns {String} 返回路径字符串
+ */
+function createDateCidDir(cid) {
+  return path.join(moment().format('YYYY/MM'), cid);
+}
+
+/**
+ * 创建带日期的新文件名
+ * @param {Object} file 文件数据
+ * @returns {String} 返回文件名
+ */
+function createNewDateName(file) {
+  var ext = mime.extension(file.headers['content-type']);
+  var newDateName = Date.now().toString() + '.' + ext;
+  return newDateName;
 }
 
 /**
@@ -188,7 +275,10 @@ function moveFilesFromTempDirToBackupDir(files, callback) {
  * @param {Function} callback 形式为function (err) {}
  */
 function createAndSaveFileModel(fileDatas, callback) {
-  // TODO
+  async.map(fileDatas, function (fileData, mapCallback) {
+    var file = new File(fileData);
+    file.save(mapCallback);
+  }, callback);
 }
 
 
