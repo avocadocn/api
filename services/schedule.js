592ed73ea1dd7cbebbb891d49b5d8627dcadb233
Maggie <maggie03230@gmail.com>
2015-05-25 18:28:43 +0800

    改善排行榜定是脚本的代码

--- services/schedule.js
+++ services/schedule.js
@@ -184,7 +184,63 @@ var teamPoint = function(){
 }
 //根据小组积分算排名
 var teamRank = exports.teamRank =function(){
-  var now = new Date()
+  var now = new Date();
+  
+  var updateTeamRank = function(region, city, group) {
+    //虚拟组不计算
+    if(group._id=="0"){
+      //找出所有同类型的正常小队并进行排名
+      return;
+    }
+    CompanyGroup.find({'active':true,'company_active':{'$ne':false},'city.province':region.name,'city.city': city.name,gid:group._id})
+    .sort('-score_rank.score -score.total -score.win_percent -score.campaign -score.member')
+    .exec(function (err,teams) {
+      var rank = new Rank();
+      rank.group_type ={
+        _id:group._id,
+        name:group.group_type
+      }
+      rank.city = {
+        province: region.name,
+        city: city.name
+      }
+      //将前十个放入rank里
+      teams.forEach(function (team,index) {
+        team.score_rank.rank = index+1;
+        if(index<rankLimit){
+          var competitionCount = team.score_rank.win + team.score_rank.tie + team.score_rank.lose;
+          rank.team.push({
+            _id: team._id,
+            cid: team.cid,
+            name: team.name,
+            cname: team.cname,
+            logo: team.logo,
+            member_num: team.member.length,
+            activity_score: team.score.total,
+            score: team.score_rank.score,
+            rank: index+1,
+            win: team.score_rank.win,
+            tie: team.score_rank.tie,
+            lose: team.score_rank.lose
+          });
+        }
+        team.save(function (err) {
+          if(err){
+            console.log(err)
+          }
+        })
+      });
+
+      if(rank.team.length>0){
+        rank.save(function (err) {
+          if(err){
+            console.log(err)
+          }
+        })
+      }
+    });
+  }
+
   async.waterfall([
     function(callback) {
       Group.find({active:true}).exec(callback);
@@ -192,56 +248,9 @@ var teamRank = exports.teamRank =function(){
     function(groups, callback) {
       Region.find().exec(function (err,regions) {
         regions.forEach(function (region) {
-          region.city.forEach(function(city){
-            groups.forEach(function(group){
-              //虚拟组不计算
-              if(group._id!="0"){
-                CompanyGroup.find({'active':true,'city.province':region.name,'city.city': city.name,gid:group._id}).sort('-score_rank.score -score.total -score.win_percent -score.campaign -score.member').exec(function (err,teams) {
-                  var rank = new Rank();
-                  rank.group_type ={
-                    _id:group._id,
-                    name:group.group_type
-                  }
-                  rank.city = {
-                    province: region.name,
-                    city: city.name
-                  }
-                  teams.forEach(function (team,index) {
-                    team.score_rank.rank = index+1;
-                    if(index<rankLimit){
-                      var competitionCount = team.score_rank.win + team.score_rank.tie + team.score_rank.lose;
-                      rank.team.push({
-                        _id: team._id,
-                        cid: team.cid,
-                        name: team.name,
-                        cname: team.cname,
-                        logo: team.logo,
-                        member_num: team.member.length,
-                        activity_score: team.score.total,
-                        score: team.score_rank.score,
-                        rank: index+1,
-                        win: team.score_rank.win,
-                        tie: team.score_rank.tie,
-                        lose: team.score_rank.lose
-                      });
-                    }
-                    team.save(function (err) {
-                      if(err){
-                        console.log(err)
-                      }
-                    })
-                  });
-
-                  if(rank.team.length>0){
-                    rank.save(function (err) {
-                      if(err){
-                        console.log(err)
-                      }
-                    })
-                  }
-                });
-              }
-
+          region.city.forEach(function (city){
+            groups.forEach(function (group){
+              updateTeamRank(region, city, group);
             });
           });
         })
