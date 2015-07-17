e7c24810150e799a951f30282d1bd2d3577471bf
ytoon <yangtongbill@gmail.com>
2015-07-17 10:26:24 +0800

    修改加入群组、退出群组的路由

--- routes/v2_0/group.js
+++ routes/v2_0/group.js
@@ -6,8 +6,8 @@ module.exports = function(app, ctrl) {
 	app.post('/groups', token.needToken, ctrl.v2_0.getFormDataForGroup, ctrl.v2_0.uploadLogoForGroup, ctrl.v2_0.createGroup); // 发新群组 
 	app.put('/groups/:groupId', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.getFormDataForUpdateGroup, ctrl.v2_0.uploadLogoForGroup, ctrl.v2_0.updateGroup); // 编辑群组
 	app.post('/groups/:groupId/invite', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.inviteMemberToGroup); // 群组邀请
-	app.put('/groups/:groupId/joinGroup', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.joinGroup); // 加入群组
-	app.delete('/groups/:groupId/quitGroup', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.quitGroup); // 退出群组
+	app.put('/groups/:groupId/user', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.joinGroup); // 加入群组
+	app.delete('/groups/:groupId/user', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.quitGroup); // 退出群组
 	app.delete('/groups/:groupId/user/:userId', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.removeMemberFromGroup); // 移除群组
 	app.put('/groups/:groupId/user/:userId', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.reassignLeaderForGroup); // 指定新群主
 	app.delete('/groups/:groupId', token.needToken, ctrl.v2_0.getGroupById, ctrl.v2_0.deleteGroup); // 删除群组
